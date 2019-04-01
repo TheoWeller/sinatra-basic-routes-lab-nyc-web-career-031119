c4c61d6f9ccbaefb4d87ba4d18058e589434afd8
Theo Weller <weller.theodor@gmail.com>
2019-04-01 20:51:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/name' do
+  "My name is Thaddy."
+end
+
+get '/homepage' do
+  "My hometown is Thaddyland."
+end
+
+get '/favorite-song' do
+  "Thaddyland."
+end
+
 end
